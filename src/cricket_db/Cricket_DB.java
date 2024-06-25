cb1146e5ade7a36f330b872e608b53a770021d6c
Bhavithh <bhavithkulal516@gmail.com>
2024-06-25 10:02:57 +0530

    First Commit

---
+++ src/cricket_db/Cricket_DB.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cricket_db;
+
+/**
+ *
+ * @author Bhavith
+ */
+public class Cricket_DB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         VLogin page = new VLogin();
+         page.setVisible(true);
+    }
+    
+}
